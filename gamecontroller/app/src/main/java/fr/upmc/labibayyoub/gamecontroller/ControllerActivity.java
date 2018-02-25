a90ab8eef7e7bf05b97024f8df85a106a280bc65
labibayyoub <labib.ayyoub@gmail.com>
2018-02-25 18:24:40 +0100

    app activities and xml (design)

--- gamecontroller/app/src/main/java/fr/upmc/labibayyoub/gamecontroller/ControllerActivity.java
+++ gamecontroller/app/src/main/java/fr/upmc/labibayyoub/gamecontroller/ControllerActivity.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ControllerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_controller);
     }
 
-    public void onButtonClick(View v){
-        Intent i =  new Intent(this, ControllerActivity.class);
-        startActivity(i);
+    public void disconnect(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        finish();
     }
 }
