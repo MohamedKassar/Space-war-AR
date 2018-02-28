c9d2bdb0cc8ff28544e91d4166c42aff219c9fe0
labibayyoub <labib.ayyoub@gmail.com>
2018-02-28 19:58:27 +0100

    Socket connexion problem solved

--- gamecontroller/app/src/main/java/fr/upmc/labibayyoub/gamecontroller/ControllerActivity.java
+++ gamecontroller/app/src/main/java/fr/upmc/labibayyoub/gamecontroller/ControllerActivity.java
@@ -1,8 +1,8 @@
 package fr.upmc.labibayyoub.gamecontroller;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +14,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ControllerActivity extends AppCompatActivity {
+
 
     private Socket sc;
     BufferedWriter out;
@@ -26,6 +26,7 @@ public class ControllerActivity extends AppCompatActivity {
     public static final String START = "START";
     public static final String STOP = "STOP";
     public static final String PAUSE = "PAUSE";
+    private static final String RESUME = "RESUME";
     public static final String RIGHT = "R";
     public static final String LEFT = "L";
     public static final String S_STOP = "S STOP";
@@ -33,12 +34,14 @@ public class ControllerActivity extends AppCompatActivity {
     public static final String SWITCH_D = "SWITCH D";
     public static final String GO = "GO";
     public static final String GG = "GG";
+    public static final String SCORE = "SCORE";
     private String SCORE_REGULAR_EXPRESSION = "SCORE +[0-9]+";
     private String GG_REGULAR_EXPRESSION = "GG +[0-9]+";
     private Button left;
     private Button right;
     private Button pause;
     private TextView score_tv;
+    private boolean onPause = false;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -46,71 +49,80 @@ public class ControllerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
 
-            if ((sc = Client.getSocket()) == null)
-                finish();
-            left = findViewById(R.id.left);
-            right = findViewById(R.id.right);
-            pause = findViewById(R.id.pause);
-            score_tv = findViewById(R.id.score);
-            score_tv.setText("0");
-            left.setOnTouchListener((v, e) ->  (onRelease(v, e)));
-            right.setOnTouchListener((v, e) -> (onRelease(v, e)));
-            new Thread(()->{
-                try {
-                    out = new BufferedWriter(new OutputStreamWriter(sc.getOutputStream()));
-                    in = new BufferedReader(new InputStreamReader(sc.getInputStream()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                runOnUiThread(()->{
-                    sendCommands(START);
-                    receiveCommands();
-                });
+        if ((sc = Client.getSocket()) == null)
+            finish();
+        left = findViewById(R.id.left);
+        right = findViewById(R.id.right);
+        pause = findViewById(R.id.pause);
+        score_tv = findViewById(R.id.score);
+        score_tv.setText("0");
+        left.setOnTouchListener((v, e) -> (onRelease(v, e, true)));
+        right.setOnTouchListener((v, e) -> (onRelease(v, e, false)));
+        new Thread(() -> {
+            try {
+                out = new BufferedWriter(new OutputStreamWriter(sc.getOutputStream()));
+                in = new BufferedReader(new InputStreamReader(sc.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            runOnUiThread(() -> sendCommands(START));
+            receiveCommands();
 
-            }).start();
 
+        }).start();
 
 
     }
 
-    private void receiveCommands()  {
-        while (true) {
-            final String[] msg = new String[1];
-            new Thread(() -> {
-                try {
-                    msg[0] = in.readLine();
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+    private void receiveCommands() {
+
+        while (!sc.isClosed()) {
+            final String msg;
+            try {
+                if ((msg = in.readLine()) == null)
+                    return;
+                switch (msg) {
+                    case SWITCH_D:
+                        runOnUiThread(() -> right.setEnabled(!right.isEnabled()));
+                        break;
+                    case SWITCH_G:
+                        runOnUiThread(() -> left.setEnabled(!left.isEnabled()));
+                        break;
+                    case GO:
+                        runOnUiThread(() -> showMessage("Game over"));
+                        break;
+
+                    default:
+                        if (MainActivity.MatchString(SCORE_REGULAR_EXPRESSION, msg)
+                                || MainActivity.MatchString(GG_REGULAR_EXPRESSION, msg)) {
+                            score_tv.setText(msg.split(" ")[1]);
+                            if (msg.split(" ")[0].equalsIgnoreCase(GG))
+                                runOnUiThread(() -> showMessage("You won !!!!!"));
+                        }
+                        break;
                 }
-            }).start();
-            switch (msg[0]) {
-                case SWITCH_D:
-                    right.setEnabled(!right.isEnabled());
-                case SWITCH_G:
-                    left.setEnabled(!left.isEnabled());
-                case GO:
-                    showMessage("Game over");
-                default:
-                    if(MainActivity.MatchString(SCORE_REGULAR_EXPRESSION,msg[0])
-                            || MainActivity.MatchString(GG_REGULAR_EXPRESSION,msg[0])) {
-                        score_tv.setText(msg[0].split(" ")[1]);
-                        if(msg[0].split(" ")[0].equalsIgnoreCase(GG))
-                            showMessage("You won !!!!!");
-                    }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
     public void showMessage(String msg) {
-        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
     }
 
-    public boolean onRelease(View v, MotionEvent event)  {
+    public boolean onRelease(View v, MotionEvent event, boolean l) {
         try {
             if (event.getAction() == MotionEvent.ACTION_UP) {
                 sendCommands(S_STOP);
+            } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                if (l)
+                    sendCommands(LEFT);
+                else
+                    sendCommands(RIGHT);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             showMessage(INTERNAL_ERROR);
         }
@@ -119,25 +131,39 @@ public class ControllerActivity extends AppCompatActivity {
 
 
     public void disconnect(View v) {
-        try {
-            sendCommands(STOP);
-            if (sc != null) {
-                sc.close();
-            }
-            finish();
-        }catch (Exception e){
-            e.printStackTrace();
-            showMessage(INTERNAL_ERROR);
-        }
-    }
-
-    public void sendCommands(String msg)  {
+        sendCommands(STOP);
         new Thread(() -> {
             try {
-                out.write(msg);
+                Thread.sleep(500);
+                if (sc != null) {
+                    sc.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            runOnUiThread(() -> finish());
+        }).start();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+
+        super.onDestroy();
+    }
+
+    public void sendCommands(String msg) {
+        showMessage(msg);
+        new Thread(() -> {
+            try {
+                out.write(msg + "\n");
+                out.flush();
             } catch (IOException e) {
                 e.printStackTrace();
-                showMessage(INTERNAL_ERROR);
+                runOnUiThread(() -> showMessage(INTERNAL_ERROR));
+
             }
         }).start();
 
@@ -145,22 +171,22 @@ public class ControllerActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-            disconnect(new View(this.getBaseContext()));
+        disconnect(new View(this.getBaseContext()));
     }
 
-    public void turnRight(View view) {
-        sendCommands(LEFT);
-    }
+    public void pause(View view) {
 
-    public void turnLeft(View view)  {
-            sendCommands(RIGHT);
-    }
+        left.setEnabled(!left.isEnabled());
+        right.setEnabled(!right.isEnabled());
+        if (onPause) {
+            sendCommands(START);
+            pause.setText(PAUSE);
+        } else {
+            sendCommands(STOP);
+            pause.setText(RESUME);
+        }
 
-    public void pause(View view)  {
-            sendCommands(PAUSE);
-            left.setEnabled(false);
-            right.setEnabled(false);
-            pause.setText("resume");
+        onPause = !onPause;
     }
 
 
--- gamecontroller/app/src/main/java/fr/upmc/labibayyoub/gamecontroller/MainActivity.java
+++ gamecontroller/app/src/main/java/fr/upmc/labibayyoub/gamecontroller/MainActivity.java
@@ -8,9 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -24,23 +22,12 @@ public class MainActivity extends AppCompatActivity {
     private int port;
     private String IP_REGULAR_EXPRESSION = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
     private Button connect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         connect = findViewById(R.id.connect);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        try {
-            if (sc != null && sc.isConnected())
-                sc.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        super.onStop();
     }
 
     public void onButtonClick(View v) {
@@ -51,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
             showMessage("Ip address is empty");
         else if (et2.getText().toString().isEmpty())
             showMessage("Port is empty");
-        else if (!MatchString(IP_REGULAR_EXPRESSION,hostName))
+        else if (!MatchString(IP_REGULAR_EXPRESSION, hostName))
             showMessage("Ip address is invalid");
         else {
             port = Integer.parseInt(et2.getText().toString());
-            connect.setEnabled(false);
+
             Thread serverThread = new Thread(new ConnexionThread());
             serverThread.start();
         }
@@ -64,15 +51,18 @@ public class MainActivity extends AppCompatActivity {
     class ConnexionThread implements Runnable {
         public void run() {
             try {
-
                 sc = new Socket();
-                 new Timer().schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        runOnUiThread(() -> connect.setEnabled(true));
-                    }
-                }, 5000);
-                sc.connect(new InetSocketAddress(hostName,port), 5000);
+                runOnUiThread(() -> {
+                    connect.setEnabled(false);
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(() -> connect.setEnabled(true));
+                        }
+                    }, 5000);
+                });
+
+                sc.connect(new InetSocketAddress(hostName, port), 5000);
                 Client.setSocket(sc);
                 runOnUiThread(() -> {
                     Intent i = new Intent(MainActivity.this, ControllerActivity.class);
@@ -80,16 +70,8 @@ public class MainActivity extends AppCompatActivity {
                 });
 
             } catch (Exception e) {
-                e.printStackTrace();
+                e.printStackTrace(System.out);
                 runOnUiThread(() -> showMessage("Couldn't establish connexion"));
-
-            } finally {
-                try {
-                    if (sc != null && sc.isConnected())
-                        sc.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         }
     }
@@ -103,4 +85,6 @@ public class MainActivity extends AppCompatActivity {
         Matcher m = p.matcher(text);
         return m.matches();
     }
+
+
 }
